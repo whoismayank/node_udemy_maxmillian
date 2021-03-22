6f6ab5ec0afb7ba96f1bf20dab9511475a3b37a3
mahendrachoudharyin <51509905+mahendrachoudharyin@users.noreply.github.com>
2021-03-22 23:13:51 +0530

    send html file, create route, create views, use path function of node js

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
+const adminRouter = require('./routes/admin');
+const shopRouter = require('./routes/shop');
+// const bodyParser = require('body-parser');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// // for parsing application/json
+// app.use(bodyParser.json()); 
+// // for parsing application/xwww-
+// app.use(bodyParser.urlencoded({ extended: true })); 
 
-app.use((req, res, next)=>{
-    console.log('This is the first middle ware.');
-    next();
-});
+// router to custom routers
+app.use('/admin',adminRouter);
+app.use(shopRouter);
 
-app.use((req, res, next)=>{
-    console.log('This is the another middle ware.');
-    // res.type('html')
-    // res.status(200);
-    res.send('<h1 style="text-align: center;">Hello from express</h1>');
+
+// manage 404 request
+app.use((req,res,next)=>{
+    res.status(404).sendFile(
+        path.join(__dirname,'/','views','404.html')
+    )
 })
+
+
 
 app.listen(3000,()=>{
     console.log('App is runnig on the port 3000');
 })
 
+// 
